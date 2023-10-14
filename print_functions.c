e3fd6f1e6cfcdc7c02f1990b677296a30e78e701
Myrna97 <136834237+Myrna97@users.noreply.github.com>
2023-10-15 01:49:50 +0300

    Update print_functions.c

--- print_functions.c
+++ print_functions.c
@@ -1,12 +1,4 @@
 #include "main.h"
-
-/**
- * print_char - prints character
- * @ap: argument pointer
- * @params: the parameters struct
- *
- * Return: number chars printed
- */
 int print_char(va_list ap, params_t *params)
 {
 	char pad_char = ' ';
